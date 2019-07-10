73c5981fcb68ae8aff08a6ff7cd64b120edc0cb0
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-07-10 14:49:21 -0500

    adding initial site files

---
+++ assets/javascript/game.js
@@ -0,0 +1,113 @@
+var initialImage = "assets/images/scifishows.jpg";
+var shows = [{
+    name: "Dark Matter",
+    themeSong: "https://www.youtube.com/embed/HC5ZDtbKIeM?controls=0" 
+}, {
+    name: "Orphan Black",
+    themeSong:
+}, {
+    name: "Star Trek: Voyager",
+    themeSong:
+}, {
+    name: "Torchwood",
+    themeSong:
+}, {
+    name: "Farscape",
+    themeSong:
+}, {
+    name: "Futurama",
+    themeSong:
+}, {
+    name: "Bablyon 5",
+    themeSong:
+}, {
+    name: "The Expanse",
+    themeSong:
+}, {
+    name: "Westworld",
+    themeSong:
+}, {
+    name: "Stranger Things",
+    themeSong:
+}, {
+    name: "Mystery Science Theater 3000",
+    themeSong:
+}, {
+    name: "Fringe",
+    themeSong:
+}, {
+    name: "Star Trek: Deep Space Nine",
+    themeSong:
+}, {
+    name: "Stargate SG-1",
+    themeSong:
+}, {
+    name: "Lost",
+    themeSong:
+}, {
+    name: "Firefly",
+    themeSong:
+}, {
+    name: "Star Trek",
+    themeSong:
+}, {
+    name: "The Twilight Zone",
+    themeSong:
+}, {
+    name: "Battlestar Galactica",
+    themeSong:
+}, {
+    name: "Star Trek: The Next Generation",
+    themeSong:
+}, {
+    name: "The X-Files",
+    themeSong:
+}, {
+    name: "Doctor Who",
+    themeSong:
+}, {
+    name: "Stargate Atlantis",
+    themeSong:
+}, {
+    name: "Stargate Universe",
+    themeSong:
+}, {
+    name: "Killjoys",
+    themeSong:
+}, {
+    name: "Buffy the Vampire Slayer",
+    themeSong:
+}, {
+    name: "Angel",
+    themeSong:
+}, {
+    name: "Defiance",
+    themeSong:
+}, {
+    name: "Doll House",
+    themeSong:
+}, {
+    name: "The Outer Limits",
+    themeSong:
+}, {
+    name: "Quantum Leap",
+    themeSong:
+}, {
+    name: "Continuum",
+    themeSong:
+}, {
+    name: "The Man in the High Castle",
+    themeSong:
+},{
+    name: "The Orville",
+    themeSong: 
+},{
+    name: "Warehouse 13",
+    themeSong: 
+},{
+    name: "Eureka",
+    themeSong: 
+},{
+    name: "Game of Thrones",  // 37 to here
+    themeSong: 
+}];
